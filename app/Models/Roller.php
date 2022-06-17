4a50079432de32b4796d0238ff02043d7be74cfc
Cihat <chtsngn61@gmail.com>
2022-06-17 10:08:30 +0300

    Rol cache temizleme

--- app/Models/Roller.php
+++ app/Models/Roller.php
@@ -4,8 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\Permission\Models\Role;
+use Spatie\Permission\Traits\RefreshesPermissionCache;
 
 class Roller extends Role
 {
-    use SoftDeletes;
+    use SoftDeletes, RefreshesPermissionCache;
 }
