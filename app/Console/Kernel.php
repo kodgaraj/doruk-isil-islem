3ca272d71526a34cd320f5dad1a8a08a7e68e045
Cihat SENGUN <chtsngn61@gmail.com>
2022-08-02 10:47:55 +0300

    Cron dakika başı olarak değiştirildi

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -28,7 +28,7 @@ class Kernel extends ConsoleKernel
         $schedule->command('islem-termin-bildirim:send')
             ->timezone('Europe/Istanbul')
             ->sendOutputTo(storage_path('logs/islem-termin-bildirim.log'))
-            ->dailyAt('08:05');
+            ->everyMinute();
     }
 
     /**
